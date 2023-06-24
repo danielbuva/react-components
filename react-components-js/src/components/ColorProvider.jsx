c300b392b0a20073f33532fa01998d2bb975db16
dani <daniel.valdecantos@gmail.com>
2023-06-24 13:34:03 -0700

    feat: add theme to local storage

--- react-components-js/src/components/ColorProvider.jsx
+++ react-components-js/src/components/ColorProvider.jsx
@@ -3,16 +3,23 @@ import { useState } from "react";
 
 export function ColorProvider({ children }) {
   const [color, setColor] = useState("");
-  const [theme, setTheme] = useState("light");
+  const [theme, setTheme] = useState(
+    localStorage.getItem("theme") ?? "light"
+  );
 
   const setToLightMode = () => {
     setTheme("light");
+    localStorage.setItem("theme", "light");
   };
+
   const setToDimMode = () => {
     setTheme("dim");
+    localStorage.setItem("theme", "dim");
   };
+
   const setToBlackMode = () => {
     setTheme("black");
+    localStorage.setItem("theme", "black");
   };
 
   return (
